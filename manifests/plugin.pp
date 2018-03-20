47f38691536c250e8e7b03dffa70da220c852c84
docEmmetBrown <gw@as2.io>
2018-03-20 11:19:23 +0100

    add plugins

---
+++ manifests/plugin.pp
@@ -0,0 +1,29 @@
+# presto::plugin
+#
+# A description of what this defined type does
+#
+# @summary A short summary of the purpose of this defined type.
+#
+# @example
+#   presto::plugin { 'namevar': }
+
+define presto::plugin(
+  $directory,
+  $url,
+  $checksum = undef,
+  $checksum_type = undef
+
+) {
+
+  archive { $title :
+    ensure        => present,
+    extract       => true,
+    extract_path  => '/opt/presto/plugin/',
+    source        =>  $url
+    checksum      => $checksum
+    checksum_type => $checksum_type
+    creates       => "/opt/presto/plugin/${title}",
+    cleanup       => true,
+  }
+
+}
